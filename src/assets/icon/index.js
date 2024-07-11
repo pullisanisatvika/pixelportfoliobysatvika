a8d04f011bdcade5297160a0a2e2527ee89ac388
pullisanisatvika <94276010+pullisanisatvika@users.noreply.github.com>
2024-07-11 10:51:03 +0530

    Add files via upload

---
+++ src/assets/icon/index.js
@@ -0,0 +1,49 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import aws from './aws.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import car from './car.svg'
+import estate from './estate.svg'
+import pricewise from './pricewise.svg'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.svg'
+import threads from './threads.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import sql from './sql.svg'
+import docker from './docker.svg'
+import springboot from './springboot.svg'
+export {
+    css,
+    git,
+    github,
+    html,
+    javascript,
+    nodejs,
+    react,
+    linkedin,
+    twitter,
+    car,
+    estate,
+    pricewise,
+    snapgram,
+    summiz,
+    threads,
+    arrow,
+    contact,
+    soundon,
+    soundoff,
+    springboot,
+    sql, 
+    docker,
+    aws
+}
